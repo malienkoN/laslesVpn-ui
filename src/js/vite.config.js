d50db45c9d4c69f0c86d68959fc0611faf5bf544
Valentin <wallpaperlowup@gmail.com>
2024-02-20 23:05:14 +0200

    fix previous commit

--- src/js/vite.config.js
+++ src/js/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
-import { createVuePlugin } from 'vite-plugin-vue2';
 
 export default defineConfig({
     plugins: [
-        createVuePlugin(), // Если вы используете Vue.js
     ],
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: `@import "@/path/to/your/variables.scss";`, // Опционально, чтобы импортировать глобальные переменные или миксины
+                additionalData: `@import "@/src/styles/main.scss";`, // Опционально, чтобы импортировать глобальные переменные или миксины
             },
         },
     },
